c550fcb4afafccc748669f4f3dcd3bb71591e904
Farell Kresnanda <kresnandaf@gmail.com>
2025-05-02 16:14:36 +0700

    Update index.js

--- node-deploy-test/index.js
+++ node-deploy-test/index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.end("Hello from Node.js CI/CD Test App!");
 });
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

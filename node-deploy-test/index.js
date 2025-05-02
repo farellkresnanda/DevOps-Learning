3a1e4774aff4556dce1bf5822788c16ac122e7b0
Farell Kresnanda <kresnandaf@gmail.com>
2025-05-02 16:02:39 +0700

    Initial Commit

---
+++ node-deploy-test/index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Node.js CI/CD Test App!");
+});
+
+const PORT = process.env.PORT || 3500;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
